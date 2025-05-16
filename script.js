e6f2adbf75114bf092dcf063c1a90cd702802918
dss-neto <192475519+dss-neto@users.noreply.github.com>
2025-05-16 07:59:22 -0300

    Fix a bug and remove unused content
    Fix: Row and column wins in last move possible were counting as a draw

--- script.js
+++ script.js
@@ -105,22 +105,17 @@ const TicTacToeLogic = (function () {
 
           DomManipulation.writeSvg(target, mark);
           DomManipulation.changeClass(target, mark);
-          console.log(`Next turn: ${PlayerObject.PlayerCircle.name}'s.`);
           // Player one round
         } else {
           mark = "O";
           board[row][column] = mark;
           DomManipulation.writeSvg(target, mark);
           DomManipulation.changeClass(target, mark);
-          console.log(`Next turn: ${PlayerObject.PlayerX.name}'s.`);
         }
         turnCounter++;
 
-        console.log(board);
-
         GameRules.getWinner(board);
       } else {
-        console.log("Try a different spot");
       }
       return mark;
     };
@@ -183,19 +178,10 @@ const TicTacToeLogic = (function () {
       for (i of winConditionsArray) {
         if (i.victory) {
           if (i.winnerMarker === "X") {
-            console.log(`The winner is ${PlayerObject.PlayerX.name}!`);
             domXScoreCounter.innerHTML = ++PlayerObject.PlayerX.score;
           } else {
-            console.log(`The winner is ${PlayerObject.PlayerCircle.name}!`);
             domCircleScoreCounter.innerHTML = ++PlayerObject.PlayerCircle.score;
           }
-
-          console.log(
-            `${PlayerObject.PlayerX.name}'s score: ${PlayerObject.PlayerX.score}`
-          );
-          console.log(
-            `${PlayerObject.PlayerCircle.name}'s score: ${PlayerObject.PlayerCircle.score}`
-          );
           DomManipulation.writeWinner(i.winnerMarker);
           gameResultDialog.showModal();
 
@@ -204,6 +190,7 @@ const TicTacToeLogic = (function () {
             Gameboard.clearBoard();
             turnCounter = 1;
           });
+          break;
         } else {
           const isDraw = (board) => {
             if (
@@ -218,7 +205,6 @@ const TicTacToeLogic = (function () {
           };
           if (isDraw(board)) {
             DomManipulation.writeWinner();
-            console.log("It's a draw!");
             gameResultDialog.showModal();
             nextRoundButton.addEventListener("click", (e) => {
               gameResultDialog.close();
@@ -245,7 +231,6 @@ playAnonymouslyButton.addEventListener("click", (e) => {
 
 gridContainer.addEventListener("click", (e) => {
   const target = e.target;
-  console.log(target.id);
   if (target.classList.contains("unmarked")) {
     if (target.id === "a") {
       TicTacToeLogic.GameRules.markBoard(0, 0, target);
@@ -286,31 +271,6 @@ playerNameForm.addEventListener("submit", (e) => {
   const circleName = PlayerNameObject.PlayerCircleName;
   TicTacToeLogic.PlayerObject.PlayerX.name = xName;
   TicTacToeLogic.PlayerObject.PlayerCircle.name = circleName;
-  console.log(TicTacToeLogic.PlayerObject);
 
   TicTacToeLogic.DomManipulation.writeNames(xName, circleName);
 });
-
-// document.addEventListener("keypress", () => {
-//   namesDialog.close();
-// });
-
-// function writeSvg(target, mark) {
-//   if (mark === "X") {
-//     target.innerHTML =
-//       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 460.775 460.775"><path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path></svg>';
-//   } else {
-//     target.innerHTML =
-//       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle-outline</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"/></svg>';
-//   }
-// }
-
-// function changeClass(target, mark) {
-//   if (mark === "X") {
-//     target.setAttribute("class", "mark-spots marked x");
-//   } else {
-//     target.setAttribute("class", "mark-spots marked circle");
-//   }
-// }
-
-// TicTacToeLogic.GameRules.selectPlayerNames();
